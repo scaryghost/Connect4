378a9d7680b42eb3741c5d9af12609258c5a6528
Eric Tsai <thegod0fde4th@gmail.com>
2012-01-21 17:36:25 -0800

    Changed applet to groovy script

--- src/com/github/etsai/connect4/canvas/GraphicsCanvasApplet.groovy
+++ src/com/github/etsai/connect4/canvas/GraphicsCanvasApplet.groovy
@@ -7,6 +7,7 @@ package com.github.etsai.connect4.canvas;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.List;
 import javax.swing.JApplet;
 
 /**
@@ -14,16 +15,19 @@ import javax.swing.JApplet;
  * @author etsai
  */
 public class GraphicsCanvasApplet extends JApplet {
+    private List<List<Integer>> board;
     final static Color bg = Color.white;
     final static Color fg = Color.black;
     final static BasicStroke stroke = new BasicStroke(2.0f);
 
+    @Override
     public void init() {
         //Initialize drawing colors
         setBackground(bg);
         setForeground(fg);
     }
     
+    @Override
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -32,5 +36,9 @@ public class GraphicsCanvasApplet extends JApplet {
         g2.setStroke(stroke);
         g2.draw(new Ellipse2D.Double(640, 360, 100, 100));
         
+    }
+    
+    public void updateBoard(List<List<Integer>> board) {
+        this.board= board;
     }
 }
