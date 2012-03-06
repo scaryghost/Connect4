39a6856c84f65b5bfa25980fb174a625044bda41
Eric Tsai <thegod0fde4th@gmail.com>
2012-03-05 22:17:50 -0800

    Modified code in main to use the Environment functions

--- src/com/github/etsai/connect4/Connect4Main.java
+++ src/com/github/etsai/connect4/Connect4Main.java
@@ -4,6 +4,9 @@
  */
 package com.github.etsai.connect4;
 
+import com.github.etsai.connect4.canvas.*;
+import java.util.Random;
+
 /**
  *
  * @author etsai
@@ -13,8 +16,17 @@ public class Connect4Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        System.out.println("Hello World");
+        Random rndGen= new Random(123456789);
+        Environment myEnv= new Environment();
+        Canvas myCanvas= new GraphicsCanvas();
+        
+        
+        for(int i= 0; i < 15; i++) {
+            int checker= (i%2 == 0 ? Environment.CHECKER_BLACK : Environment.CHECKER_RED);
+            myEnv.addChecker(rndGen.nextInt(Environment.MAX_COLS),checker);
+        }
+        myEnv.drawBoard(myCanvas);
     }
 }
