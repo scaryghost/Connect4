3befbef0310deb4920a1ecd9b37d679c480628e2
Eric Tsai <thegod0fde4th@gmail.com>
2012-03-05 23:35:29 -0800

    Added skeleton file for GameType class

---
+++ src/com/github/etsai/connect4/GameType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.etsai.connect4;
+
+/**
+ *
+ * @author etsai
+ */
+public class GameType {
+
+}
