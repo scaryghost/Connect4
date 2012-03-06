b5f95d4a314541029c07f98d126e031f9737c353
Eric Tsai <thegod0fde4th@gmail.com>
2012-03-05 23:13:12 -0800

    Added controller interface

---
+++ src/com/github/etsai/connect4/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.etsai.connect4;
+
+/**
+ *
+ * @author etsai
+ */
+public interface Controller {
+    public void move(Environment env);
+}
---
+++ src/com/github/etsai/connect4/controller/PlayerController.groovy
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.etsai.connect4.controller
+
+import com.github.etsai.connect4.*
+
+/**
+ *
+ * @author etsai
+ */
+class PlayerController implements Controller {
+    public void move(Environment env) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
+
