faa6a0835aff99ebe7b10612cd4c75572eb43b0f
Eric Tsai <thegod0fde4th@gmail.com>
2012-03-06 23:44:34 -0800

    Cleaned up the graphics canvas paint code

--- src/com/github/etsai/connect4/canvas/GraphicsCanvasApplet.groovy
+++ src/com/github/etsai/connect4/canvas/GraphicsCanvasApplet.groovy
@@ -21,14 +21,17 @@ import javax.swing.JApplet
 public class GraphicsCanvasApplet extends JApplet implements MouseListener, MouseMotionListener {
     private def cursorRowCol= null
     private def isButtonPressed
-    final static def stroke = new BasicStroke(2.0f);
-
+    private final static def stroke= new BasicStroke(2.0f)
+    private static def checkerColorMap= [(Environment.CHECKER_BLACK):Color.black,
+        (Environment.CHECKER_RED):Color.red, (Environment.CHECKER_EMPTY):Color.white]
+    private static def previewColor= Color.lightGray
+    
     @Override
     public void init() {
         //Initialize drawing colors
         setBackground(Color.white);
-        addMouseListener( this );
-        addMouseMotionListener( this );
+        addMouseListener(this);
+        addMouseMotionListener(this);
     }
     
     @Override
@@ -44,24 +47,18 @@ public class GraphicsCanvasApplet extends JApplet implements MouseListener, Mous
         (0 ..< Environment.MAX_ROWS).each {row ->
             (0 ..< Environment.MAX_COLS).each {col ->
                 def checker= coreInstance.env.getChecker(row,col)
-                if (checker == Environment.CHECKER_BLACK) {
-                    g2.setColor(Color.black)
-                    g2.fill(new Ellipse2D.Double(100*(col+1), 100*row+50, 75, 75))
-                } else if (checker == Environment.CHECKER_RED) {
-                    g2.setColor(Color.red)
-                    g2.fill(new Ellipse2D.Double(100*(col+1), 100*row+50, 75, 75))
-                } else if (checker == Environment.CHECKER_EMPTY) {
+                def checkerColor
+                
+                checkerColor= checkerColorMap[checker]
+                if (checker == Environment.CHECKER_EMPTY) {
                     if (cursorRowCol != null && 
                         cursorRowCol[0] == row && cursorRowCol[1] == col) {
-                        g2.setColor(Color.gray)
-                        g2.fill(new Ellipse2D.Double(100*(col+1), 100*row+50, 75, 75))
+                        checkerColor= previewColor
                         cursorRowCol= null
-                    } else {
-                        g2.setColor(Color.white)
-                        g2.fill(new Ellipse2D.Double(100*(col+1), 100*row+50, 75, 75))
                     }
-                    
                 }
+                g2.setColor(checkerColor)
+                g2.fill(new Ellipse2D.Double(100*(col+1), 100*row+50, 75, 75))
             }
         }
         
