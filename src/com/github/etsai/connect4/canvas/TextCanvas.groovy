38c06fa00da862fa73e07ef974d69490f767d54c
Eric Tsai <thegod0fde4th@gmail.com>
2012-03-06 23:35:18 -0800

    Prettied the text canvas output

--- src/com/github/etsai/connect4/canvas/TextCanvas.groovy
+++ src/com/github/etsai/connect4/canvas/TextCanvas.groovy
@@ -12,6 +12,8 @@ import com.github.etsai.connect4.*
  * @author etsai
  */
 class TextCanvas implements Canvas {
+    private static def checkerTextMap= [(Environment.CHECKER_BLACK):"*",
+        (Environment.CHECKER_RED):"O", (Environment.CHECKER_EMPTY):"_"]
     public TextCanvas() {
     }
     public void drawBoard() {
@@ -21,7 +23,7 @@ class TextCanvas implements Canvas {
         (0 ..< Environment.MAX_ROWS).each {row ->
             rowString= ""
             (0 ..< Environment.MAX_COLS).each {col ->
-                rowString+= coreInstance.env.getChecker(row,col)
+                rowString+= checkerTextMap[coreInstance.env.getChecker(row,col)]+"\t"
             }
             println rowString
         }
